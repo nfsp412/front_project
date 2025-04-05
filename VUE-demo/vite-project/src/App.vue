8dfc1b2c886eed6dab14c7781a6a4ac2a345b6d2
AsukaCoder <nfsp412@gmail.com>
2025-04-05 22:47:28 +0800

    vue3+vite响应式编程 插值表达式

--- VUE-demo/vite-project/src/App.vue
+++ VUE-demo/vite-project/src/App.vue
@@ -7,6 +7,25 @@ function increase() {
 function decrease() {
   counter.value--;
 }
+
+// 插值表达式
+let msg = "hello vue3";
+let getMsg = () => {
+  return "msg1";
+};
+let age = 19;
+let bee = "蜜 蜂";
+const carts = [
+  { name: "可乐", price: 3, number: 10 },
+  { name: "薯片", price: 6, number: 8 },
+];
+function compute() {
+  let cnt = 0;
+  for (let index in carts) {
+    cnt += carts[index].price * carts[index].number;
+  }
+  return cnt;
+}
 </script>
 
 <template>
@@ -14,6 +33,13 @@ function decrease() {
     <button @click="decrease()">-</button>
     {{ counter }}
     <button @click="increase()">+</button>
+
+    <h1>{{ msg }}</h1>
+    {{ getMsg() }} <br />
+    {{ age >= 18 ? "true" : "false" }} <br />
+    {{ bee.split(" ").reverse().join("=") }} <br />
+    {{ compute() }} <br />
+    {{ carts[0].price * carts[0].number }} <br />
   </div>
 </template>
 
