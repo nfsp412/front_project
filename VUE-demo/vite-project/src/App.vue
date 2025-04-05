0df7d6f2653956046b5fff8ed6277e6ff6cb8add
AsukaCoder <nfsp412@gmail.com>
2025-04-05 22:36:44 +0800

    vue3+vite响应式编程 写法

--- VUE-demo/vite-project/src/App.vue
+++ VUE-demo/vite-project/src/App.vue
@@ -1,30 +1,33 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script type="module">
+import { ref } from "vue";
+export default {
+  setup() {
+    let counter = ref(1);
+    function increase() {
+      counter.value++;
+    }
+    function decrease() {
+      counter.value--;
+    }
+    return {
+      counter,
+      increase,
+      decrease,
+    };
+  },
+};
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <button @click="decrease()">-</button>
+    {{ counter }}
+    <button @click="increase()">+</button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+button {
+  border: 1px solid red;
 }
 </style>
