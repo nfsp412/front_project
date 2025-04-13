2961662b673ef142ccfef94d670dd0db85dd6d00
AsukaCoder <nfsp412@gmail.com>
2025-04-13 22:39:19 +0800

    Pinia 2

--- Pinia/vite-project/src/store/store.js
+++ Pinia/vite-project/src/store/store.js
@@ -3,6 +3,7 @@ import { defineStore } from "pinia";
 // Uncaught (in promise) TypeError: Cannot destructure property 'state' of 'options' as it is undefined.
 //id属性写在{}外面
 export const definedPerson = defineStore("personPinia", {
+  // state是核心内容 是一个返回初始状态的函数
   state: () => {
     return {
       name: "zs",
@@ -10,14 +11,20 @@ export const definedPerson = defineStore("personPinia", {
       hobbies: ["sing", "run"],
     };
   },
+  //   getters等同于state的计算值
+  //   推荐使用箭头函数并且传入state形参
   getters: {
-    getAge() {
-      return this.age;
+    getAge: (state) => {
+      return state.age;
     },
-    getHobbiesCount() {
-      return this.hobbies.length;
+    getHobbiesCount: (state) => {
+      return state.hobbies.length;
     },
   },
+  //   actions相当于方法method
+  // 类似于getter
+  // 可以直接通过this访问
+  // 可以是异步的,使用await
   actions: {
     doubleAge() {
       this.age *= 2;
