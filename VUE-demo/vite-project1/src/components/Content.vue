7daf7e2170391d111e1f20f691d3f09bbb0fd523
AsukaCoder <nfsp412@gmail.com>
2025-04-13 16:24:36 +0800

    vue3+vite响应式编程  组件化

---
+++ VUE-demo/vite-project1/src/components/Content.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script setup type="module">
+import { defineProps } from "vue";
+defineProps({
+  msg: String,//和 msg 名称一致
+});
+</script>
+
+<style scoped></style>
---
+++ VUE-demo/vite-project1/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    欢迎: xx <a href="#">退出登录</a><br />
+    {{ navi_msg }}
+  </div>
+</template>
+
+<script setup type="module">
+import { defineProps } from "vue";
+defineProps({
+  navi_msg: String,
+});
+</script>
+
+<style scoped></style>
